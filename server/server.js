020d55edf73890419c3c98f5a38ff85ccf45b9bb
hoya <wh_hacker@naver.com>
2021-07-19 16:01:08 +0900

    20210719_add book model error handling middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ require("dotenv").config();
 //var, let과는 다르게 const키워드는 선언과 동시에 리터럴 값을 할당해주어야하며, 재할당이 불가능하다
 const express = require("express"); 
 const connectToDB=require("./database/db");
+const ErrorsMiddleware= require("./middleware/errorMiddleware");
+const LibraryError = require("./utils/libraryError")
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught Exception...... stopping the server.....");
+    console.log(error.name, error.message);
+    
+    process.exit(1) 
+});
 
 //Express 모듈을 실행해 app 변수에 할당, 익스프레스 내부에 http모듈이 내장되어 있으므로 서버의 역할을 할 수 있다.
 const app = express(); 
@@ -26,10 +35,26 @@ app.get("/test", (req, res)=> {
     });
 });
 
+app.all("*", (req,res,next) => {
+    next ( 
+        new LibraryError( `Can't find ${req.originalUrl} on this server!`, 404)
+    );
+});
+app.use(ErrorsMiddleware);
+
 //listen 포트를 연결하고 서버를 실행
-app.listen(
+//app.listen(
+const server = app.listen( 
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}` 
     )
 ); 
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection...... stopping the server.....");
+    console.log(error.name, error.message);
+    server.close(() => {
+            process.exit(1) 
+    });
+});
